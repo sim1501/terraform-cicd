82ce0b893ff3c4d55baf9146324dbe1d7bc261cc
Simran Arora <simran.arora@databricks.com>
2022-09-16 17:52:11 +0530

    Working Module

--- tf_files/databricks/main.tf
+++ tf_files/databricks/main.tf
@@ -15,20 +15,20 @@ terraform {
 provider "databricks" {
   # profile = var.databricks_connection_profile
   account_id = ""
-  host = "https://adb-8596220602934265.5.azuredatabricks.net"
-  token = var.token
-  }
+  host       = "https://adb-8596220602934265.5.azuredatabricks.net"
+  token      = var.token
+}
 
 
 
-resource "databricks_group" "group" {
+/*resource "databricks_group" "group" {
     allow_cluster_create       = true
     allow_instance_pool_create = true
     databricks_sql_access      = true
     display_name               = "testing_simran"
     workspace_access           = true
     force		               = true
-}
+}*/
 
 
 # resource "databricks_group_instance_profile" "all" {
@@ -38,7 +38,7 @@ resource "databricks_group" "group" {
 
 resource "databricks_sql_query" "q1" {
   data_source_id = "676432a6-c8bd-4bab-9e3b-7e8e51085f64"
-  name           = "My Query Name"
+  name           = "My Working Project"
   query          = "SELECT * from simran_test.customers"
 
 }
@@ -50,6 +50,81 @@ resource "databricks_sql_visualization" "q1v1" {
   description = "Some Description"
 
   // The options encoded in this field are passed verbatim to the SQLA API.
-  options = jsonencode(file("/Users/simran.arora/terraformrepo/terraform-cicd/tf_files/databricks/rpt_viz.json"))
-  
+  options = jsonencode({
+
+    "alignYAxesAtZero" : false,
+    "coefficient" : 1,
+    "columnConfigurationMap" : {
+      "series" : {
+        "column" : "AccountId",
+        "id" : "column_7915eaf48"
+      },
+      "x" : {
+        "column" : "vertical",
+        "id" : "column_7915eaf47"
+      },
+      "y" : [
+        {
+          "column" : "dollar_ammount",
+          "id" : "column_7915eaf49",
+          "transform" : "SUM"
+        }
+      ]
+    },
+    "dateTimeFormat" : "YYYY-MM-DD HH:mm",
+    "direction" : {
+      "type" : "counterclockwise"
+    },
+    "error_y" : {
+      "type" : "data",
+      "visible" : true
+    },
+    "globalSeriesType" : "pie",
+    "legend" : {
+      "traceorder" : "normal"
+    },
+    "missingValuesAsZero" : true,
+    "numberFormat" : "0,0[.]00000",
+    "percentFormat" : "0[.]00%",
+    "series" : {
+      "error_y" : {
+        "type" : "data",
+        "visible" : true
+      },
+      "stacking" : "stack"
+    },
+    "seriesOptions" : {
+      "column_7915eaf49" : {
+        "name" : "dollar_ammount",
+        "type" : "pie",
+        "yAxis" : 0
+      }
+    },
+    "showDataLabels" : true,
+    "sizemode" : "diameter",
+    "sortX" : true,
+    "sortY" : true,
+    "swappedAxes" : false,
+    "textFormat" : "",
+    "useAggregationsUi" : true,
+    "valuesOptions" : {},
+    "version" : 2,
+    "xAxis" : {
+      "labels" : {
+        "enabled" : true
+      },
+      "type" : "-"
+    },
+    "yAxis" : [
+      {
+        "type" : "-"
+      },
+      {
+        "opposite" : true,
+        "type" : "-"
+      }
+    ]
+
+  })
+
 }
--- tf_files/databricks/variable.tf
+++ tf_files/databricks/variable.tf
@@ -1,11 +1,11 @@
 variable "databricks_connection_profile" {
   description = "The name of the Databricks connection profile to use."
-  type = string
-  default = "DEFAULT"
+  type        = string
+  default     = "DEFAULT"
 }
 
-variable token{
-    description = "The token to use for the connection."
-    type = string
-    default= "dapid2912dd705d6b6100c72d9adaa5699d0-3"
+variable "token" {
+  description = "The token to use for the connection."
+  type        = string
+  default     = "dapid2912dd705d6b6100c72d9adaa5699d0-3"
 }
